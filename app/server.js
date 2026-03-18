a15411dd412e977a5af4534b160c6d8166a09cbd
root <root@alikalilinuxvm.alikalilinuxvm>
2026-03-18 08:11:46 -0400

    first commit Pushing code to github

---
+++ app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Blog Platform Running 🚀</h1>');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
